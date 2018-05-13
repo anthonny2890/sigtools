a5328db21c37fc2afd001e9b34e11fa64513383f
Anthonny Alanoca <aalanoca@innovatetools.com>
2018-05-13 11:00:38 -0500

    ok

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialBaseComponent } from './material-base/material-base.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  } from '@angular/material';
+//import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   declarations: [MaterialBaseComponent]
 })
